2144fcb89f748e9fe2e9161296957d48a8597c6e
Carlos <carlosng1993@gmail.com>
2023-01-25 11:47:13 +0100

    feat: TypographyFeed added

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -25,6 +25,36 @@ const Feed = () => {
 				© 2023 CNG Media
 			</Typography>
 		</Box>
+
+		<Box
+			p={2}
+			sx={{
+				overflowY: 'auto',
+				height: '90vh',
+				flex: '2'
+			}}
+		>
+			<Typography
+				variant='h4'
+				fontWeight='bold'
+				mb={2}
+				sx={{
+					color: 'white'
+				}}
+			>
+				New
+				
+				<span
+					style={{
+						color: '#F31503'
+					}}
+				>
+					videos
+				</span>
+			</Typography>
+
+			<Videos />
+		</Box>
 	</Stack>
   )
 }
